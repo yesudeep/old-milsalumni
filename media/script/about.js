a1a84c96b0f23b068bd66cc6a133c3d3d7ce02fd
Aswad Rangnekar <aswad.r@gmail.com>
2010-06-27 19:32:51 +0530

    add picture frame to about page and pushed pending html files
    Signed-off-by: Aswad Rangnekar <aswad.r@gmail.com>

--- media/script/about.js
+++ media/script/about.js
@@ -1,3 +1,29 @@
-jQuery(function(){
+/*jQuery(function(){
+   
+    jQuery(function(){
+    /*jQuery("#about_alumni").innerfade({
+      animationtype: 'fade',
+      speed: 'slow',
+      timeout: 12000,
+      type: 'sequence',
+      containerheight: '250px'
+});*/
 
+jQuery(function(){
+    /*jQuery("#slider").easySlider({
+        numeric: true,
+        speed: 200
+    });
+    jQuery('#items').scrollable({size: 1}).navigator({
+        navi: "item_numbers",
+        naviItem: 'a',
+        activeClass: 'current'
+    });*/
+    jQuery("#item_numbers").tabs("#items > li", {
+          effect: 'horizontal',
+          //fadeOutSpeed: "slow",
+          rotate: true
+    }).slideshow({
+          autoplay: true
+    });
 });
